d17524718ec7875f21c923260128e52035d9d30b
GerardoCM717 <gerardocm717@gmail.com>
2025-06-11 22:26:57 -0600

    HistorialPagos

---
+++ src/app/services/historial-pagos.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HistorialPago } from '../models/historial-pago.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HistorialPagosService {
+  private readonly apiUrl = 'http://192.168.68.68:3001/api/historial_pagos';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<HistorialPago[]> {
+    return this.http.get<HistorialPago[]>(this.apiUrl);
+  }
+
+  getByGrupo(grupoId: number): Observable<HistorialPago[]> {
+    return this.http.get<HistorialPago[]>(`${this.apiUrl}?grupoId=${grupoId}`);
+  }
+
+  getByUsuario(userId: number): Observable<HistorialPago[]> {
+    return this.http.get<HistorialPago[]>(
+      `${this.apiUrl}?userId=${userId}`
+    );
+  }
+}
